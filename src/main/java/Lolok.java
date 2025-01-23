dde3ee869876562897ab353fb05a0140c6f34be8
zihung20 <zihung20@gmail.com>
2025-01-23 11:40:31 +0800

    Add echo features

--- src/main/java/Lolok.java
+++ src/main/java/Lolok.java
@@ -1,7 +1,10 @@
+import java.util.Scanner;
+
 public class Lolok {
     private final String name = "Lolok";
     private final String logo = "";
     private void greet() {
+        printLine();
         System.out.println("Hello! I'm " + this.name);
         System.out.println("What can I do for you?");
     }
@@ -12,13 +15,28 @@ public class Lolok {
 
     private void exit() {
         System.out.println("Bye. Hope to see you again soon!");
+        printLine();
+    }
+
+    private void echo(String message) {
+        printLine();
+        System.out.println(message);
+        printLine();
     }
     public static void main(String[] args) {
        Lolok lolok = new Lolok();
-       printLine();
+
        lolok.greet();
-       printLine();
-       lolok.exit();
-       printLine();
+        Scanner scanner = new Scanner(System.in);
+       while (true) {
+           String input = scanner.next();
+           if(input.equals("bye")) {
+               printLine();
+               lolok.exit();
+               break;
+           } else {
+               lolok.echo(input);
+           }
+       }
     }
 }
