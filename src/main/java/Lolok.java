819681ca6e2b583b4a552bcf7b0f101c2e990caf
zihung20 <zihung20@gmail.com>
2025-01-23 11:55:40 +0800

    Add ability to store user text

--- src/main/java/Lolok.java
+++ src/main/java/Lolok.java
@@ -1,8 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Lolok {
     private final String name = "Lolok";
     private final String logo = "";
+    private List<String> list = new ArrayList<>();
+
     private void greet() {
         printLine();
         System.out.println("Hello! I'm " + this.name);
@@ -18,25 +22,46 @@ public class Lolok {
         printLine();
     }
 
+    private void getUserInput() {
+        //https://stackoverflow.com/questions/39514730/how-to-take-input-as-string-with-spaces-in-java-using-scanner
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine().toLowerCase();
+            if(input.equals("bye")) {
+                printLine();
+                this.exit();
+                break;
+            } else if(input.equals("list")){
+                this.printList();
+            } else {
+                this.addToList(input);
+            }
+        }
+        scanner.close();
+    }
     private void echo(String message) {
         printLine();
         System.out.println(message);
+        printLine();
+    }
+
+    private void addToList(String message) {
+        this.echo("added: " + message);
+        this.list.add(message);
+    }
+
+    private void printList() {
+        printLine();
+        for(int i = 0; i < list.size(); i++) {
+            System.out.print(i + ". ");
+            System.out.println(list.get(i));
+        }
         printLine();
     }
     public static void main(String[] args) {
        Lolok lolok = new Lolok();
 
        lolok.greet();
-        Scanner scanner = new Scanner(System.in);
-       while (true) {
-           String input = scanner.next();
-           if(input.equals("bye")) {
-               printLine();
-               lolok.exit();
-               break;
-           } else {
-               lolok.echo(input);
-           }
-       }
+       lolok.getUserInput();
     }
 }
