a8048325d39a7a04051e88d62c539e60fa329e2c
zihung20 <zihung20@gmail.com>
2025-01-28 17:29:28 +0800

    Created file for ENUM feature

--- src/main/java/Action.java
+++ src/main/java/Action.java
@@ -1,3 +1,18 @@
 public enum Action {
-    LIST, MARK, UNMARK, TODO, DEADLINE, EVENT, DELETE
+    LIST(0),
+    MARK(1),
+    UNMARK(1),
+    TODO(1),
+    DEADLINE(2),
+    EVENT(2),
+    DELETE(1);
+
+    private final int argumentCount;
+    Action(int argumentCount) {
+        this.argumentCount = argumentCount;
+    }
+
+    public int getArgumentCount() {
+        return this.argumentCount;
+    }
 }
