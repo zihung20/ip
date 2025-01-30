60f981c0032928774369d418248573d4b5d2c391
zihung20 <zihung20@gmail.com>
2025-01-30 19:07:44 +0800

    Move command related method to Command class

--- src/main/java/Command.java
+++ src/main/java/Command.java
@@ -1,99 +1,15 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Command {
     private String[] blocks;
     private String type;
+    private boolean isExit = false;
 
     public Command(String[] block) {
         this.blocks = block;
         this.type = block[0];
-    }
-    private String[] todoArgument() {
-        String[] ans = new String[1];
-        StringBuilder builder = new StringBuilder();
-        for(int i = 1; i < blocks.length; i++) {
-            if(!builder.isEmpty()) {
-                builder.append(" ");
-            }
-            builder.append(blocks[i]);
-        }
-        ans[0] = builder.toString();
-        return ans;
-    }
-
-    private String[] deadlineArgument() {
-        String[] ans = new String[2];
-        int i = 1;
-        StringBuilder builder = new StringBuilder();
-        while(i < blocks.length && !blocks[i].equals("/by")) {
-            if(!builder.isEmpty()) {
-                builder.append(" ");
-            }
-            builder.append(blocks[i]);
-            i++;
-        }
-        ans[0] = builder.toString();
-        i++;
-        builder = new StringBuilder();
-        while(i < blocks.length) {
-            if(!builder.isEmpty()) {
-                builder.append(" ");
-            }
-            builder.append(blocks[i]);
-            i++;
-        }
-        ans[1] = builder.toString();
-        return ans;
-    }
-
-    private String[] eventArgument() {
-        String[] ans = new String[3];
-        int i = 1;
-        StringBuilder builder = new StringBuilder();
-        while(i < blocks.length && !blocks[i].equals("/from")) {
-            if(!builder.isEmpty()) {
-                builder.append(" ");
-            }
-            builder.append(blocks[i]);
-            i++;
-        }
-        ans[0] = builder.toString();
-        i++;
-        builder = new StringBuilder();
-        while(i < blocks.length && !blocks[i].equals("/to")) {
-            if(!builder.isEmpty()) {
-                builder.append(" ");
-            }
-            builder.append(blocks[i]);
-            i++;
-        }
-        ans[1] = builder.toString();
-        i++;
-        builder = new StringBuilder();
-        while (i < blocks.length) {
-            if(!builder.isEmpty()) {
-                builder.append(" ");
-            }
-            builder.append(blocks[i]);
-            i++;
-        }
-        ans[2] = builder.toString();
-        return ans;
-    }
-
-    public String[] getArgument() {
-        switch (type) {
-            case "todo":
-                return todoArgument();
-            case "deadline":
-                return deadlineArgument();
-            case "event":
-                return eventArgument();
-            default:
-                return new String[]{};
-                //exception
-        }
     }
 
     public String[] getArgument (int count) throws IncorrectArgumentNumberException{
@@ -124,8 +40,50 @@ public class Command {
         return this.type;
     }
 
-    public static void main(String[] args) {
-        Command c = new Command("mark 1".split(" "));
-        System.out.println(Arrays.toString(c.getArgument(3)));
+    public void executeCommand(TaskList taskList, Ui ui, Storage storage) {
+        switch (blocks[0]) {
+        case "bye":
+            isExit = true;
+            break;
+        case "list":
+            taskList.printList();
+            break;
+        default:
+            multipleArgumentCommand(taskList);
+        }
+    }
+
+    private void multipleArgumentCommand(TaskList taskList){
+        try {
+            String type = this.getType();
+            //https://www.geeksforgeeks.org/list-contains-method-in-java-with-examples/
+            if(List.of("mark", "unmark", "delete").contains(type)) {
+                String[] arg = this.getArgument(1);
+                if(type.equals("delete")) {
+                    taskList.deleteTask(Integer.parseInt(arg[0]) - 1);
+                } else {
+                    taskList.markTask(Integer.parseInt(arg[0]) - 1, this.getType().equals("mark"));
+                }
+            } else if(type.equals("todo")) {
+                String[] arg = this.getArgument(1);
+                taskList.addToList(new Todo(arg[0]));
+            } else if(type.equals("deadline")){
+                String[] arg = this.getArgument(2);
+                taskList.addToList(new Deadline(arg[0], arg[1]));
+            } else if (type.equals("event")) {
+                String[] arg = this.getArgument(3);
+                taskList.addToList(new Event(arg[0], arg[1], arg[2]));
+            } else {
+                throw new InvalidCommandException(type);
+            }
+        } catch (InvalidCommandException e) {
+            Ui.printMessageBlock(e.toString());
+        } catch (IncorrectArgumentNumberException e) {
+            Ui.printMessageBlock(e.toString());
+        }
+    }
+
+    public boolean isExit() {
+        return this.isExit;
     }
 }
--- src/main/java/Lolok.java
+++ src/main/java/Lolok.java
@@ -5,21 +5,17 @@ public class Lolok {
     private final String name = "Lolok";
     private Storage storage;
     private TaskList taskList;
+    private Command command;
+    private Ui ui;
 
     public Lolok(String path) {
         readData(path);
+        this.ui = new Ui();
     }
 
     public void run() {
-        this.greet();
+        ui.greet(this.name);
         this.getUserInput();
-    }
-
-    private void greet() {
-        Ui.printLine();
-        System.out.println("Hello! I'm " + this.name);
-        System.out.println("What can I do for you?");
-        Ui.printLine();
     }
 
     public void readData(String path) {
@@ -28,63 +24,30 @@ public class Lolok {
     }
 
     private void exit() {
-        Ui.printLine();
         this.storage.saveData(taskList.getList(), false);
         System.out.println("Bye. Hope to see you again soon!");
         Ui.printLine();
     }
 
     private void getUserInput() {
-        //https://stackoverflow.com/questions/39514730/how-to-take-input-as-string-with-spaces-in-java-using-scanner
-        try (Scanner scanner = new Scanner(System.in)) {
-            boolean exit = false;
-            while (!exit) {
-                //lowercase can add in the future
-                String input = scanner.nextLine();
+        boolean exit = false;
+        while (!exit) {
+            try {
+                String input = ui.readCommand();
+                Ui.printLine();
                 Command command = new Command(input.split(" "));
-                switch (input) {
-                case "bye":
-                    exit = true;
-                    this.exit();
-                    break;
-                case "list":
-                    taskList.printList();
-                    break;
-                default:
-                    multipleArgumentCommand(command);
+                command.executeCommand(taskList, ui, storage);
+                exit = command.isExit();
+            } catch (LolokException e) {
+                // should already handle exception in Command
+                System.out.println(e.toString());
+            } finally {
+                if(!exit) {
+                    Ui.printLine();
                 }
             }
-        } catch (LolokException e) {
-            System.err.println(e);
         }
-        //clean up
-    }
-    private void multipleArgumentCommand(Command command) throws InvalidCommandException{
-        try {
-            String type = command.getType();
-            //https://www.geeksforgeeks.org/list-contains-method-in-java-with-examples/
-            if(List.of("mark", "unmark", "delete").contains(type)) {
-                String[] arg = command.getArgument(1);
-                if(type.equals("delete")) {
-                    taskList.deleteTask(Integer.parseInt(arg[0]) - 1);
-                } else {
-                    taskList.markTask(Integer.parseInt(arg[0]) - 1, command.getType().equals("mark"));
-                }
-            } else if(type.equals("todo")) {
-                String[] arg = command.getArgument(1);
-                taskList.addToList(new Todo(arg[0]));
-            } else if(type.equals("deadline")){
-                String[] arg = command.getArgument(2);
-                taskList.addToList(new Deadline(arg[0], arg[1]));
-            } else if (type.equals("event")) {
-                String[] arg = command.getArgument(3);
-                taskList.addToList(new Event(arg[0], arg[1], arg[2]));
-            } else {
-                throw new InvalidCommandException(type);
-            }
-        } catch (LolokException e) {
-            Ui.printMessageBlock(e.toString());
-        }
+        this.exit();
     }
 
     public static void main(String[] args) {
